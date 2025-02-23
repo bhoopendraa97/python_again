69da2f45de4ce066e777a72fd7da3b30419a9b6c
bhoopendraa97 <bhoopendraa77@gmail.com>
2025-02-23 14:19:37 +0530

    decorate

---
+++ decorate.py
@@ -0,0 +1,50 @@
+
+'''def   outer_function (fun):
+     
+        def inner_function(x,y):
+            x=x+5
+            y=y+5
+            data=fun(x,y)
+            return data
+        return inner_function
+
+@outer_function
+def main_function(x,y):
+      z=x+y
+      return z
+data=main_function(5,10)
+print(data)'''
+
+'''def outer_function(fun):
+       def inner_function(x,y):
+                x=x+5 
+                y=y+6
+                data=fun(x,y)
+                return data
+       return inner_function
+
+@outer_function
+def main_function(x,y):
+         z=x+y
+         return z
+data=main_function(5,10)
+print(data)'''
+
+
+                                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
