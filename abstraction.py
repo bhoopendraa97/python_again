870cb676f7dc95769b536baa0ad41b6f07668beb
bhoopendraa97 <bhoopendraa77@gmail.com>
2025-03-05 22:18:27 +0530

    add

---
+++ abstraction.py
@@ -0,0 +1,71 @@
+
+'''class calculator:
+      def  add(self,*n):
+             sum=0
+             for i in n:
+                    sum=sum+i
+             print(sum)  
+
+      def sub(self,*n):
+             sub=0
+             for i in n:
+                    sub=sub-i
+             print(sub)  '''     
+                        
+'''class calculator:
+      def  add(*n):
+             sum=0
+             for i in n:
+                    sum=sum+i
+             print(sum)  
+
+      def sub(*n):
+             sub=0
+             for i in n:
+                    sub=sub-i
+             print(sub)  '''
+
+
+'''class calculator:
+      def  add(*n):
+             sum=0
+             for i in n:
+                    sum=sum+i
+             print(sum)  
+
+      def sub(*n):
+             sub=0
+             for i in n:
+                    sub=sub-i
+             print(sub) '''
+
+'''class calculator:
+      def add(*n):
+            sum=0
+            for i in n:
+                  sum=sum+i
+            print(sum) 
+
+      def sub(*n):
+            sub=0
+            for i in n:
+                  sub=sub-i
+            print(sub) '''              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
