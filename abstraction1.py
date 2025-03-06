75d26736e9a61ebafff9e701dccc48eca5b41337
bhoopendraa97 <bhoopendraa77@gmail.com>
2025-03-06 14:04:19 +0530

    add

---
+++ abstraction1.py
@@ -0,0 +1,25 @@
+
+from abstraction import calculator
+
+'''obj=calculator()
+obj.add(10)
+obj.add(10,20,30)
+obj.sub(10,20,30,40)'''
+
+obj=calculator()
+obj.add(10)
+obj.add(10,20,30)
+obj.sub(10,20,30,40)
+
+#from abstraction import calculator as c
+
+'''c.add(10)
+c.add(12,20,30)
+c.sub(10,20,30,40)'''
+
+
+
+
+
+
+
