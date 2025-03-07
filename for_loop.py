02c4bea0bb4e93fce58d84e07a8669bbf59ec063
bhoopendraa97 <bhoopendraa77@gmail.com>
2025-03-07 22:30:55 +0530

    for_loop

---
+++ for_loop.py
@@ -0,0 +1,190 @@
+
+'''
+for i in collection :
+1) string
+2) list
+3) tuple
+4) set
+5)dict
+6) range
+'''
+# W.A.P to print n natural number ------------------------------------------->
+'''n=int(input("enter your number"))
+for i in range(1,n+1):
+     if i<=(n-1):
+          print(i,end=",")
+     else:
+          print(i)   '''
+
+# W.A.P to print n natural number of sum----------------------------------------->
+'''n=int(input("enter your number"))
+sum=0
+for i in range(1,n+1):
+     sum=sum+i
+     if i<=(n-1):
+          print(i,end="+")
+     else:
+          print(i,end="=")     
+print(sum)'''
+
+# W.A.P to print even number using foor loo ------------------------------------>
+'''n=int(input("enter your number"))
+sum=0
+for i in range(1,n+1):
+     if i%2==0:
+            sum=sum+i
+            if i<=(n-2):
+                print(i,end="+")
+            else:
+                print(i,end="=")     
+print(sum)'''
+
+# W.A.P to print odd number sum using foor loo ------------------------------------>
+'''n=int(input("enter your number"))
+sum=0
+for i in range(1,n+1):
+     if i%2!=0:
+           sum=sum+i
+           if i<=(n-1):
+                 print(i,end="+")
+
+           else:
+                 print(i,end="=")
+
+print(sum)   '''           
+
+
+#W.A.P to print odd number of sum----------------------------->
+'''n=int(input("enter your number"))
+sum=0
+for i in range(1,n+1,2):
+     sum=sum+i
+     if i<=(n-1):
+          print(i,end="+")
+     else:
+          print(i,end="=")     
+
+print(sum)'''
+
+
+# W.A.P to print even number----------------------------------------->
+'''n=int(input("enter your number"))
+for i in range(2,n+1,2):
+     if i<=(n-2):
+          print (i,end=",")
+     else:
+          print(i)  '''
+
+
+# W.A.P to print count of even number--------------------------------------->
+'''n=int(input("enter the number"))
+for i in range(1, n+1):
+       if i<=(n-1):
+              print(2*i, end=",")
+       else:
+              print(2*i)'''
+
+
+
+
+
+
+
+# W.A.P to print count of odd number--------------------------------------->
+'''n=int(input("enter the number"))
+for i in range(1, n+1):
+       if i<=(n-1):
+              print(2*i-1, end=",")
+       else:
+              print(2*i-1)'''           
+
+
+'''n=int(input("enter the number"))
+sum=0
+for i in range(2,n+1,2):
+       if i<=n:
+           sum=sum+i
+           if i<=(n-2):
+               print(i,end="+")
+           else:
+               print(i,end="=")
+
+print(sum)'''
+
+
+      
+
+
+
+
+
+
+
+'''n=(input("enter the string"))
+v=0
+c=0
+for i in n :
+    x=['a','e','i','o','u','A','E','I','O','U']
+    if i in x:
+        v=v+1
+    else:
+        c=c+1  
+print(v)
+print(c)'''   
+
+                       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Q)8------------------------->
+
+
+   
+     
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
