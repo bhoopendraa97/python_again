d954fbdc547837ad945a79375dbdbc560bd9e895
bhoopendraa97 <bhoopendraa77@gmail.com>
2025-03-15 16:19:46 +0530

    map

---
+++ map_function.py
@@ -0,0 +1,47 @@
+
+ #Map() higher order function---------------
+
+'''my_list=[10,20,30,40]    #collection output nikal kar dena like list tuple ect----->
+
+def sqr(n):
+    return n*n
+x=map(sqr,my_list)
+print(list(x))'''
+
+
+'''my_list=[4,5,6]
+def sqe(n):
+    return n*n
+x=map(sqe,my_list)
+print(tuple(x))'''
+
+
+'''my_str="Neeraj"
+def add(n):
+      x=ord(n)
+      return chr(x+5)
+x=map(add,my_str)
+print(x)
+print(list(x))'''
+
+
+'''my_str="Neeraj"
+def add(n):
+   x=ord(n)
+   return x
+x=map(add,my_str)
+print(x)
+print(list(x))'''
+
+
+
+
+
+
+
+
+
+
+
+
+
