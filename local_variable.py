70346198755535e963f9bbe1b83555cce48c11c9
bhoopendraa97 <bhoopendraa77@gmail.com>
2025-03-16 14:18:10 +0530

    local

---
+++ local_variable.py
@@ -0,0 +1,78 @@
+
+
+# x=50
+# class student:
+#        global x
+#        x=10
+#        def add ():
+#               print(student.x)  
+#               print(x)  
+#               y=20
+#               print(y)
+#        def add1():
+#               print(x)      
+
+# obj=student
+# obj.add()
+# obj.add1()
+# print(x)
+
+
+           
+
+
+
+'''x="Danish"
+class school:
+     global x
+     x="raj"
+
+     def add():
+          print(x)
+          y="Arju"
+          print(y)
+     def add1():
+         print(x)
+
+obj=school
+obj.add()          
+obj.add1()
+print(x)'''
+
+
+'''x=50
+def new():
+    global x
+    print(x)
+    x=10
+    print(x)
+print(x)    
+new()    
+print(x)  '''
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
