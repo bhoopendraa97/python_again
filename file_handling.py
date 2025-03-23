225d0ed4fb9418c876c63cb4978f747f828f2444
bhoopendraa97 <bhoopendraa77@gmail.com>
2025-03-23 09:07:43 +0530

    add

---
+++ file_handling.py
@@ -0,0 +1,9 @@
+#permanent data stroge
+
+#type--->text file----->string data-->(text,doc,pdf)
+#Binary-file---->list,tuple,set,dict(list,tuple,set,dict(bin.exe)) ---->(image,audio,video)
+#csv-file---->key-value--->key-value)
+
+#1. open
+
+
